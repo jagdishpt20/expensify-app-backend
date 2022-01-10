db9bd3c4046d38bcf973aa99f445869e9ed86245
jagdishpt20 <68055859+jagdishpt20@users.noreply.github.com>
2022-01-10 18:20:21 +0530

    Update ExpensifyAppBackendApplicationTests.java

--- src/test/java/com/jss/expensifyappbackend/ExpensifyAppBackendApplicationTests.java
+++ src/test/java/com/jss/expensifyappbackend/ExpensifyAppBackendApplicationTests.java
@@ -2,6 +2,7 @@ package com.jss.expensifyappbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Ignore;
 
 @SpringBootTest
 class ExpensifyAppBackendApplicationTests {
